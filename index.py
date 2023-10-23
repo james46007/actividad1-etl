b8fc2b9c33b89692a46daa0766db3d8f81baab70
jimmy <jimmy1817@hotmail.com>
2023-10-22 22:25:02 -0500

    [fixed]: process ETL and upload Documento.docx

--- index.py
+++ index.py
@@ -1,3 +1,4 @@
+# pip install pandas
 import pandas as pd
 
 df = pd.read_csv('mdi_homicidiosintencionales_pm_2023_enero_agosto.csv', sep=';', encoding='latin1')
@@ -10,7 +11,6 @@ columns_object = df.select_dtypes(include=['object']).columns.tolist()
 for column in columns_object:
     df[column] = df[column].str.strip()
 
-# print(df.sample(10))
 # Imprimir los valores únicos de todas las columnas
 for column in df.columns:
     unique_values = df[column].unique()
@@ -19,30 +19,32 @@ for column in df.columns:
 # Identificar las provincias, Remplazar valores que estan mal
 df.loc[df.Provincia == "LOS RIOS", "Provincia"] = "PROVINCIA DE LOS RÍOS"
 df.loc[df.Provincia == "BOLIVAR", "Provincia"] = "PROVINCIA DE BOLÍVAR"
-# print(df["Provincia"].unique())
+
+# Convertir el campo fecha
+df['Fecha Infracción'] = pd.to_datetime(df['Fecha Infracción'], format='%d/%m/%Y', errors='ignore')
+df['Fecha Infracción'] = df['Fecha Infracción'].dt.date
 
 # Libreria para instalar
 # pip install SQLAlchemy
 # pip install pymysql
-import pandas as pd
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine
 
-# Credentials to database connection
+# Credenciales para la conexión de la base de datos
 hostname = "209.145.61.41"
 dbname = "actividad1"
 uname = "usr_maintainer"
 pwd = "Tc2;1EE{DBE^oN"
 
-# Create SQLAlchemy engine to connect to MySQL Database
+# Cree el motor SQLAlchemy para conectarse a la base de datos MySQL
 engine = create_engine("mysql+pymysql://{user}:{pw}@{host}/{db}"
                        .format(host=hostname, db=dbname, user=uname, pw=pwd))
 
-# Assuming you want to exclude the column named 'unwanted_column'
+# Suponiendo que desea excluir la columna denominada 'columna_no_deseada'
 columns_to_exclude = ['Cod_prov', 'cod_cantón', 'Med. Edad']
 columns_to_keep = [column for column in df.columns if column not in columns_to_exclude]
 
-# Create a new DataFrame without the unwanted column
+# Cree un nuevo DataFrame sin la columna no deseada
 filtered_df = df[columns_to_keep]
 
-# Convert dataframe to sql table
+# Convertir marco de datos a tabla SQL
 filtered_df.to_sql('deber', engine, if_exists='replace', index=False)
